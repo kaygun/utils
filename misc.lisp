162fc13346dccdb80aab5ea2ab234095c4b9fa05
Atabey Kaygun <atabey_kaygun@hotmail.com>
2015-12-19 05:43:36 +0200

    Moved some functions into 'misc' category.

---
+++ misc.lisp
@@ -0,0 +1,39 @@
+(in-package :utils)
+
+(defun concurrent-map (fs xs)
+  (mapcar (lambda (x) (mapcar (lambda (f) (funcall f x))
+                              fs))
+          xs))
+
+(defun concurrent-reduce (rs inits xs)
+  (mapcar (lambda (r i) (reduce r xs :initial-value i))
+          rs inits))
+
+
+(defun write-dot (G filename &optional (directed nil))
+  (progn 
+     (with-open-file (dot-file (format nil "~A.dot" filename) 
+ 			       :direction :output 
+			       :if-exists :supersede
+			       :if-does-not-exist :create)
+       (if directed
+ 	   (format dot-file "digraph G {~% node[shape=circle];~% ~{ ~{\"~A\" -> \"~A\"; ~} ~% ~} ~%}~%" G)
+	   (format dot-file "graph G {~% node[shape=circle]; ~% ~{ ~{\"~A\" -- \"~A\"; ~} ~% ~} }~%" G)))
+     (uiop:run-program 
+          (format nil "circo -T png -o ~A.png ~:*~A.dot" filename))
+     (format nil "![](~a.png)~%" filename)))
+
+
+(defun data-plot (filename &optional (title ""))
+  (let ((command (format nil "set terminal png; ~
+                              set output '~a.png'; ~
+                              plot '~:*~a.csv' with lines title '~a`; " filename title)))
+    (uiop:run-program (format nil "gnuplot -e \"~a\"" command))
+    (format nil "![](~a.png)~%" filename)))
+
+
+(defun r-command (command)
+  (cl-ppcre:regex-replace-all
+   "\\n"
+   (uiop:run-program (format nil "r -e \"~a\"" command) :output :string)
+   (format nil "~%  ")))
